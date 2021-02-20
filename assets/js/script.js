efc3c711e6c6825dee26ba6c16ba0374a9b03ef1
Jake <jakech617@gmail.com>
2021-02-20 14:46:09 -0500

    fixed issue that caused 404 error when trying to view highscores after quiz completition

--- assets/js/script.js
+++ assets/js/script.js
@@ -186,7 +186,7 @@ function quizComplete() {
             var newScore = JSON.stringify(allScores);
             localStorage.setItem("allScores", newScore);
             // navigates to final page.
-            window.location.replace("./HighScores.html");
+            window.location.replace(".highscores.html");
         }
     });
 
