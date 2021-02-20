003b9664bec2e51b0cb9e3c138dd931f879a0a70
Jake <jakech617@gmail.com>
2021-02-20 14:21:33 -0500

    added assets folder with css and js files

---
+++ assets/js/highscores.js
@@ -0,0 +1,30 @@
+// global variables.
+var highScore = document.querySelector("#highScore");
+var clear = document.querySelector("#clear");
+var goBack = document.querySelector("#goBack");
+
+// event listener to clear scores.
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
+
+// retreives scores in local storage.
+var allScores = localStorage.getItem("allScores");
+allScores = JSON.parse(allScores);
+
+if (allScores !== null) {
+
+    for (var i = 0; i < allScores.length; i++) {
+
+        var createLi = document.createElement("li");
+        createLi.textContent = allScores[i].initials + " " + allScores[i].score;
+        highScore.appendChild(createLi);
+
+    }
+}
+
+// event listener to returnto main page.
+goBack.addEventListener("click", function () {
+    window.location.replace("./index.html");
+});
